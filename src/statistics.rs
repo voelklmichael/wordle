c5f17a564f9191d040b8d978c786a78a1f5efc06
Michael Völkl <michaelvoelkl@zoho.com>
2025-06-09 19:05:34 +0200

    using web time

--- src/statistics.rs
+++ src/statistics.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, Default, serde::Serialize, serde::Deserialize)]
 pub struct Statistics {
     #[serde(skip)]
-    pub last_time_start: Option<std::time::Instant>,
+    pub last_time_start: Option<web_time::Instant>,
     pub started_games: usize,
     pub won_games: Vec<(u8, Option<u32>)>, // number of guesses for each won games
     pub lost_games: Vec<Option<u32>>,      // lost games=>six guesses
@@ -9,12 +9,12 @@ pub struct Statistics {
 impl Statistics {
     pub(crate) fn new_game(&mut self) {
         self.started_games += 1;
-        self.last_time_start = Some(std::time::Instant::now());
+        self.last_time_start = Some(web_time::Instant::now());
     }
 
     pub(crate) fn game_completed(&mut self, guesses: Option<usize>) {
         let duration = if let Some(last_time_start) = self.last_time_start.take() {
-            Some((std::time::Instant::now() - last_time_start).as_secs() as u32)
+            Some((web_time::Instant::now() - last_time_start).as_secs() as u32)
         } else {
             None
         };
